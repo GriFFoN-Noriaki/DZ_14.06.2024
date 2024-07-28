97267cdaeeef1b1889aef74b0806549185b17b25
GriFFoN-Noriaki <zhabadryn@gmail.com>
2024-07-28 14:14:54 +0500

    Add files via upload

---
+++ 14.06.2024_1.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <string>
+
+struct Car 
+{
+    std::string color;
+    std::string model;
+    union {
+        int number; // пятизначный номер
+        char word[9]; // слово до 8 символов
+    } plateNumber;
+    bool isNumber; // флаг, чтобы определить тип номера
+
+    // Функция для заполнения автомобиля
+    void fillCar() 
+    {
+        setlocale(LC_ALL, "rus");
+
+        std::cout << "Введите цвет: ";
+        std::cin >> color;
+        std::cout << "Введите модель: ";
+        std::cin >> model;
+        std::cout << "Введите номер (5 цифр или до 8 символов): ";
+        std::string temp;
+        std::cin >> temp;
+        if (temp.size() == 5 && std::isdigit(temp[0])) 
+        {
+            plateNumber.number = std::stoi(temp);
+            isNumber = true;
+        }
+        else 
+        {
+            strcpy_s(plateNumber.word, temp.c_str());
+            isNumber = false;
+        }
+    }
+
+    // Функция для печати информации о машине
+    void printCar() const 
+    {
+        setlocale(LC_ALL, "rus");
+
+        std::cout << "Цвет: " << color << ", Модель: " << model << ", Номер: ";
+        if (isNumber) 
+        {
+            std::cout << plateNumber.number;
+        }
+        else 
+        {
+            std::cout << plateNumber.word;
+        }
+        std::cout << std::endl;
+    }
+};
+
+// Функции для работы с массивом машин
+void printAllCars(Car cars[], int size) 
+{
+    for (int i = 0; i < size; i++) 
+    {
+        cars[i].printCar();
+    }
+}
+
+// Пример поиска машины по номеру можно реализовать дополнительно
+
+int main() {
+    Car garage[10]; // Массив из 10 машин
+    for (int i = 0; i < 10; i++) 
+    {
+        garage[i].fillCar();
+    }
+    printAllCars(garage, 10);
+    return 0;
+}
---
+++ 14.06.2024_2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+enum class EntityType {Bird, Livestock, Human};
+
+struct Characteristic 
+{
+    union 
+    {
+        double flightSpeed; // Для птицы
+        bool isUngulate;    // Для скота
+        int iqLevel;        // Для человека
+    };
+};
+
+struct LivingEntity 
+{
+    double speed;
+    EntityType type;
+    std::string color;
+    Characteristic characteristic;
+
+    void input() 
+    {
+        setlocale(LC_ALL, "rus");
+
+        std::cout << "Введите скорость: ";
+        std::cin >> speed;
+        int typeInput;
+        std::cout << "Введите тип (0 - Птица, 1 - Скот, 2 - Человек): ";
+        std::cin >> typeInput;
+        type = static_cast<EntityType>(typeInput);
+        std::cout << "Введите цвет: ";
+        std::cin >> color;
+
+        if (type == EntityType::Bird) 
+        {
+            std::cout << "Введите скорость полета: ";
+            std::cin >> characteristic.flightSpeed;
+        }
+        else if (type == EntityType::Livestock) 
+        {
+            std::cout << "Парнокопытное? (1 - Да, 0 - Нет): ";
+            std::cin >> characteristic.isUngulate;
+        }
+        else if (type == EntityType::Human) 
+        {
+            std::cout << "Введите уровень IQ: ";
+            std::cin >> characteristic.iqLevel;
+        }
+    }
+
+    void print() 
+    {
+        setlocale(LC_ALL, "rus");
+
+        std::cout << "Скорость: " << speed << ", Тип: " << static_cast<int>(type)
+            << ", Цвет: " << color << ", ";
+        if (type == EntityType::Bird) 
+        {
+            std::cout << "Скорость полета: " << characteristic.flightSpeed << "\\n";
+        }
+        else if (type == EntityType::Livestock) 
+        {
+            std::cout << "Парнокопытное: " << (characteristic.isUngulate ? "Да" : "Нет") << "\\n";
+        }
+        else if (type == EntityType::Human) 
+        {
+            std::cout << "Уровень IQ: " << characteristic.iqLevel << "\\n";
+        }
+    }
+};
+
+int main() 
+{
+    std::vector<LivingEntity> entities(10);
+
+    // Ввод информации для всех сущностей
+    for (auto& entity : entities) 
+    {
+        entity.input();
+    }
+
+    // Печать всего списка
+    for (auto& entity : entities) 
+    {
+        entity.print();
+    }
+
+    return 0;
+}
